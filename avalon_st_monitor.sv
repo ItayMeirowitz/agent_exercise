326f93848aa03fe751156698cef1065b3e6ef612
ItayMeirowitz <Itayme96@gmail.com>
2026-03-24 12:08:33 +0200

    Fixed monitor pr issues

--- avalon_st_monitor.sv
+++ avalon_st_monitor.sv
@@ -15,14 +15,14 @@ class avalon_st_monitor #(parameter int DATA_WIDTH_IN_BYTES = 4);
         
         // Start separate thread to monitor (without halting the software)
         fork
-            monitor();
+            monitor_invalid_interface();
+            monitor_msgs();
         join_none
     endfunction
 
     /*-------------------------------------------------------------------------------
 	-- Functions and Tasks.
     -------------------------------------------------------------------------------*/
-
     // Drive master using a byte queue converting it into the avalon_st interface
     task monitor_msg(output byte_queue queue);
     
@@ -32,65 +32,79 @@ class avalon_st_monitor #(parameter int DATA_WIDTH_IN_BYTES = 4);
     
         // Is the monitor in a packet?
         bit in_packet = 1'b0;
-        bit eop_received = 1'b0;
+
+        // Error tracking vars
+        bit eop_received  = 1'b0;
         bit invalid_input = 1'b0;
 
-
+        // Listen to transactions
         do begin
 
             // Sync to clocking block
             @(vif.monitor_cb);
 
-            if (!in_packet && vif.monitor_cb.valid && !vif.monitor_cb.sop) begin
-                $display("Valid out of packet");
-                invalid_input = 1'b1;
-            end
+            // Check if transaction occurred
+            if (vif.monitor_cb.rdy && vif.monitor_cb.valid) begin
+                
+                // Check eop and sop
+                if (vif.monitor_cb.sop) begin
+                    in_packet = 1'b1;
+                end
+                if (vif.monitor_cb.eop) begin
+                    eop_received = 1'b1;
+                end
 
-            if (in_packet && vif.monitor_cb.valid && vif.monitor_cb.sop) begin
-                $display("Multiple SOPs");
-                invalid_input = 1'b1;
-            end
+                // Pack received word.
+                word_received = {>>$size(byte){vif.monitor_cb.data}};
 
-            if (!invalid_input) begin
-
-                // Check if transaction occurred
-                if (vif.monitor_cb.rdy && vif.monitor_cb.valid) begin
-                    
-                    // Check eop and sop
-                    if (vif.monitor_cb.sop) begin
-                        in_packet = 1'b1;
-                    end
-                    if (vif.monitor_cb.eop) begin
-                        eop_received = 1'b1;
-                    end
-
-                    // Pack received word.
-                    word_received = {>>$size(byte){vif.monitor_cb.data}};
-
-                    // Push word into queue, and apply empty only at EOP
-                    if (eop_received) begin
-                        received_queue = {
-                            received_queue,
-                            word_received[0 : word_received.size() - vif.monitor_cb.empty - 1]
-                        }; 
-                    end else begin
-                        // received_queue.push_back(word_received); 
-                        received_queue = {
-                            received_queue,
-                            word_received
-                        };
-                    end
+                // Push word into queue, and apply empty only at EOP
+                if (eop_received) begin
+                    received_queue = {
+                        received_queue,
+                        word_received[0 : word_received.size() - vif.monitor_cb.empty - 1]
+                    }; 
+                end else begin
+                    // received_queue.push_back(word_received); 
+                    received_queue = {
+                        received_queue,
+                        word_received
+                    };
                 end
             end
-
-            // Reset invalid input for next loop
-            invalid_input = 1'b0;
         end while (!(in_packet && eop_received));
 
+        // Set output to queue
         queue = received_queue;
     endtask
 
-    task monitor();
+    // Check for invalid avalon st interface
+    task monitor_invalid_interface();
+        forever begin
+
+            // Sync to clocking block
+            @(vif.monitor_cb);
+
+            // Check for valid out of packet (also checks multiple EOPs)
+            if (!in_packet && vif.monitor_cb.valid && !vif.monitor_cb.sop) begin
+                $fatal("Valid out of packet");
+                invalid_input = 1'b1;
+            end
+
+            // Check for multiple SOPs (without proceeding EOP)
+            if (in_packet && vif.monitor_cb.valid && vif.monitor_cb.sop) begin
+                $fatal("Multiple SOPs");
+                invalid_input = 1'b1;
+            end
+
+            // Check if empty is in valid range
+            if (vif.monitor_cb.valid && (vif.monitor_cb.empty >= DATA_WIDTH_IN_BYTES)) begin
+                $fatal("BAD empty value");
+                invalid_input = 1'b1;
+            end
+        end
+    endtask
+
+    task monitor_msgs();
         byte_queue queue;
 
         forever begin
